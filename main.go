642fdc09a7e62da1c9135cafe331a1915f64292c
Paul Cortes <paulkerg@gmail.com>
2026-03-04 11:21:40 -0500

    add logo image and refactor server configuration for better readability

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir("./")))
+	serverMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serverMux,
 	}
-	fmt.Println("Starting server on http://localhost:8080")
+	fmt.Println("Starting server on http://localhost:" + port)
 	server.ListenAndServe()
 }
