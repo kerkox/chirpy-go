1ee516b68b0154d470f3b03b58cbc8d6b7761f54
Paul Cortes <paulkerg@gmail.com>
2026-03-04 11:18:08 -0500

    add initial HTML file and set up basic HTTP server

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	serverMux := new(http.ServeMux)
+	serverMux := http.NewServeMux()
+	serverMux.Handle("/", http.FileServer(http.Dir("./")))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
 	}
-
+	fmt.Println("Starting server on http://localhost:8080")
 	server.ListenAndServe()
 }
