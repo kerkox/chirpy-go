a900bb8e79c4d7c43d68cbccb6e065661ca04fd9
Paul Cortes <paulkerg@gmail.com>
2026-03-14 12:01:03 -0500

    update API endpoint paths for health check and metrics

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func main() {
 		fileserveHits: atomic.Int32{},
 	}
 	serverMux.Handle("/app/", cfg.middlewareMetricsInc(appFileServerHandler))
-	serverMux.HandleFunc("GET /healthz", handleHealthz)
-	serverMux.HandleFunc("GET /metrics", cfg.getFileServeHits)
-	serverMux.HandleFunc("POST /reset", cfg.resetFileServeHits)
+	serverMux.HandleFunc("GET /api/healthz", handleHealthz)
+	serverMux.HandleFunc("GET /api/metrics", cfg.getFileServeHits)
+	serverMux.HandleFunc("POST /api/reset", cfg.resetFileServeHits)
 
 	server := &http.Server{
 		Addr:    ":" + port,
